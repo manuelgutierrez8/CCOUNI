b5cc7f6f7f70f736322850bba7fd61fdb7da9063
manuel.gutierrez <manuel.gutierrez@ooqia.com>
2019-08-06 23:08:01 -0600

    Adding md5 encrypt method

--- WebApp/Login.aspx.cs
+++ WebApp/Login.aspx.cs
@@ -20,15 +20,7 @@ namespace WebApp
         {
             if (Page.IsValid)
             {
-                StringBuilder hash = new StringBuilder();
-                MD5CryptoServiceProvider md5provider = new MD5CryptoServiceProvider();
-                byte[] bytes = md5provider.ComputeHash(new UTF8Encoding().GetBytes("manuel"));
-
-                for (int i = 0; i < bytes.Length; i++)
-                {
-                    hash.Append(bytes[i].ToString("x2"));
-                }
-
+                string encryptedPassword = App_Code.Utils.EncryptMD5(txtPassword.Text);
             }
         }
     }
